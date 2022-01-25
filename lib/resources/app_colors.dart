8f8b89ae7010485db69f3f0c25889bcc1b8d4cd7
Rostislav <poct2002@gmail.com>
2022-01-25 10:55:12 +0200

    [NEW] Program Archetecture

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  AppColors._();
+
+  static const Color green = Color(0xFF0FC494);
+  static const Color text = Color(0xFF133B77);
+  static const Color blue = Color(0xFF0066FF);
+  static const Color base = Color(0xFFF8FAFD);
+  static const Color notActive = Color(0xFFA7B0C0);
+  static const Color darkGreen = Color(0xFF003225);
+  static const Color button = Color(0xFFF1FDF8);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color outline = Color(0xFFEEF1F5);
+  static const Color buttonText = Color(0xFF056249);
+}
---
+++ lib/resources/app_icons.dart
@@ -0,0 +1,7 @@
+class AppIcons {
+
+  AppIcons._();
+
+  
+  
+}
