909dd0918b762956adda81443a6700f0d54adebb
Rostislav <poct2002@gmail.com>
2022-01-27 13:55:20 +0200

    [NEW] Firebase Auth Faced

---
+++ lib/services/firebase_auth_facade.dart
@@ -0,0 +1,74 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:usa_in_ua/models/auth/core/errors.dart';
+import 'package:usa_in_ua/models/auth/domain/auth_failure.dart';
+import 'package:usa_in_ua/models/auth/domain/i_auth_facade.dart';
+import 'package:usa_in_ua/models/auth/domain/value_objects.dart';
+
+class FirebaseAuthFacade implements IAuthFacade {
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  FirebaseAuthFacade(
+    this._firebaseAuth,
+    this._googleSignIn,
+  );
+
+  @override
+  Future<Either<AuthFailure, Unit>> registerWithPhoneNumber({
+    required PhoneNumber phoneNumber,
+  }) async {
+    final phoneNumberStr = phoneNumber.getOrCrash();
+    try {
+      await _firebaseAuth.verifyPhoneNumber(
+        phoneNumber: phoneNumberStr,
+        verificationCompleted: (PhoneAuthCredential credential) {},
+        verificationFailed: (FirebaseAuthException e) {},
+        codeSent: (String verificationId, int? resendToken) {},
+        codeAutoRetrievalTimeout: (String verificationId) {},
+      );
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.code == 'phone-number-already-exists') {
+        return left(const AuthFailure.phoneNumberAlreadyInUse());
+      } else {
+        return left(const AuthFailure.serverError());
+      }
+    }
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithPhoneNumberAndPassword({
+    required PhoneNumber phoneNumber,
+    required Password password,
+  }) {
+    final phoneNumberStr = phoneNumber.getOrCrash();
+    final passwordStr = password.getOrCrash();
+
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithGoogle() async {
+    try {
+      final googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        return left(const AuthFailure.cancelledByUser());
+      }
+
+      final googleAuthentication = await googleUser.authentication;
+
+      final authCredential = GoogleAuthProvider.credential(
+        idToken: googleAuthentication.idToken,
+        accessToken: googleAuthentication.accessToken,
+      );
+
+      await _firebaseAuth.signInWithCredential(authCredential);
+      return right(unit);
+    } on FirebaseAuthException catch (_) {
+      return left(const AuthFailure.serverError());
+    }
+  }
+}
